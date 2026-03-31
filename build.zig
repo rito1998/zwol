a29a152365083ee8a64209e274cdd274399c3848
Riccardo Torreggiani <riccardo.torreggiani@gmail.com>
2026-03-31 19:53:30 +0200

    remove dependency from libc

--- build.zig
+++ build.zig
@@ -32,7 +32,6 @@ pub fn build(b: *std.Build) void {
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
-            .link_libc = true,
             .imports = &.{
                 .{ .name = "wol", .module = wol },
                 .{ .name = "clap", .module = clap },
